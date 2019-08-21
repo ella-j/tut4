5d67af51606a0128e371d11a96d3453fad44006b
Ella <ella.j.bussell@gmail.com>
2019-08-21 12:45:36 +1200

    fixed Calc class

--- src/main/java/tut4CI/Calc.java
+++ src/main/java/tut4CI/Calc.java
@@ -2,14 +2,12 @@ package tut4CI;
 
 public class Calc {
 
-	//int x = 2;
-	//int y = 3;
 
-	public void add(int x, int y) {
+	public int add(int x, int y) {
 		return x+y;
 	}
 
-	public void subtract(int x, int y) {
+	public int subtract(int x, int y) {
 		return x+y;
 	}
 
